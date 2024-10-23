8cf9c503dd29111bedc118c7164ce5c930dd1d63
Surendra Rawal <surendra.rawal@ensue.com.np>
2024-10-23 21:24:50 +0545

    style changed

--- src/commons/components/ui-components/NotFound.tsx
+++ src/commons/components/ui-components/NotFound.tsx
@@ -3,7 +3,7 @@ import {AppLink, AppText, AppTitle} from "../index";
 
 export default function NotFound() {
     return (
-        <div className="absolute insect-0 index--1 flex flex-direction-column align-items-center justify-center">
+        <div className="absolute insect-0 index--1 flex flex-column align-items-center justify-center">
             <AppTitle>404</AppTitle>
             <AppText textAlign='center'>Oops! The page you're looking for can't be found. Please check the URL or
                 return to the <AppLink textDecoration='underline' to={''} color='info'>Homepage.</AppLink></AppText>
